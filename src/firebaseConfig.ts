86e9febda7c7608fc36a3de700f714d00dddba72
Stark <123723458+St4rkXc@users.noreply.github.com>
2025-03-24 23:15:24 +0800

    intial commit

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,20 @@
+// src/firebaseConfig.ts
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL:"https://esp-fetching-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+export { database }
