d8c0578c9b76dd03f6257a1481cd08b05812d45b
Stark <123723458+St4rkXc@users.noreply.github.com>
2025-04-04 14:22:52 +0800

    swap humidity and temperature data labels in HomeView; update display logic accordingly

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,8 +40,9 @@ onMounted(() => {
                 />
             </div>
             <div class="flex flex-row gap-x-4 mt-8">
+                <!-- Humidity Sensor data -->
                 <div class="w-full space-y-3 flex flex-col">
-                    <dataLabel :Data="tempSensorData ?? 0" Label="Temparature" type="celcius" class="flex-1" />
+                    <dataLabel :Data="humiditySensorData ?? 0" Label="Humidity" type="percent" class="flex-1" />
                     <div class="w-full p-6 rounded-xl shadow-sm border border-zinc-300">
                         <div class="flex justify-between items-start mb-4">
                             <p class="text-zinc-800 text-xl font-semibold">Humidity</p>
@@ -52,13 +53,14 @@ onMounted(() => {
                         <SingeLineChart :firstData="humiditySensorData" />
                     </div>
                 </div>
+                <!-- Temp Sensor Data -->
                 <div class="w-full space-y-3 flex flex-col">
-                    <dataLabel :Data="humiditySensorData ?? 0" Label="Humidity" type="percent" class="flex-1"/>
+                    <dataLabel :Data="tempSensorData ?? 0" Label="Temperature" type="celcius" class="flex-1"/>
                     <div class="w-full p-6 rounded-xl shadow-sm border border-zinc-300">
                         <div class="flex justify-between items-start mb-4">
                             <p class="text-zinc-800 text-xl font-semibold">Temp</p>
                             <div>
-                                <p class=" text-zinc rounded text-2xl font-semibold">{{ humiditySensorData }} C</p>
+                                <p class=" text-zinc rounded text-2xl font-semibold">{{ tempSensorData }} C</p>
                             </div>
                         </div>
                         <SingeLineChart :firstData="tempSensorData" />
